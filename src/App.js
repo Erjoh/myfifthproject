fbd24ab89cf671dfd099c9acb8985ab1d3f938d5
Erjoh <erjik1902@gmail.com>
2023-08-22 11:58:42 +0600

    usery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [users, setUsers] = useState([])
+    const [name, setName] = useState('')
+    const [edit , setEdit] = useState(null)
+
+    useEffect(() => {
+        axios('https://64e2ef6bbac46e480e77ed78.mockapi.io/api/vector1/users')
+            .then(({data}) => setUsers(data))
+    }, [])
+
+    const handleChange = (e, user) => {
+        const newData = {...user, hired: e.target.checked}
+        axios.put(`https://64e2ef6bbac46e480e77ed78.mockapi.io/api/vector1/users/${user.id}`, newData)
+            .then(({data}) => {
+                setUsers(users.map(user => user.id === data.id ? data : user))
+            })
+    }
+
+    const handleDelete = (user) => {
+        axios.delete(`https://64e2ef6bbac46e480e77ed78.mockapi.io/api/vector1/users/${user.id}`)
+            .then(({data}) => {
+                setUsers(users.filter(user => user.id !== data.id))
+            })
+    }
+
+    const handleAddUser = (e, user) => {
+        e.preventDefault()
+        if (!!edit?.id){
+            axios.put(`https://64e2ef6bbac46e480e77ed78.mockapi.io/api/vector1/users/${edit.id}`, {name})
+                .then(({data}) => {
+                    setUsers(users.map(user => user.id === data.id ? data : user))
+                    setName('')
+                    setEdit(null)
+                })
+        } else {
+            axios.post(`https://64e2ef6bbac46e480e77ed78.mockapi.io/api/vector1/users/`, {name})
+                .then(({data}) => {
+                    setUsers([...users, data])
+                    setName('')
+                })
+        }
+    }
+
+    const handleEdit = (user) => {
+        setName(user.name)
+        setEdit(user)
+    }
+
+    return (
+        <div className={'container'}>
+            <div>
+                <form className={'search-wrapper'} onSubmit={handleAddUser}>
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text"/>
+                    <button type={'submit'}>{edit ? 'Edit user-name' : 'Add user-name'}</button>
+                </form>
+            </div>
+            {
+                users.map(user => {
+                    return (
+                        <div key={user.id} className={'user-wrapper'}>
+                            <h2>{user.name}</h2>
+                            <p>{user.email}</p>
+                            <input type="checkbox" onChange={(e) => handleChange(e, user)} checked={user.hired}/>
+                            <button onClick={() => handleDelete(user)}>Delete</button>
+                            <button onClick={() => handleEdit(user)} >Edit</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+};
 
 export default App;
